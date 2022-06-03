0e18f363adf04e4a29d08cf2b8ddfc80c99317cc
Shrey <63983735+shrey45@users.noreply.github.com>
2022-06-03 09:18:05 -0400

    Add files via upload

---
+++ Code/sketch_jun02a.ino
@@ -0,0 +1,92 @@
+#include <Servo.h>
+
+const int flexPin = A0; //pin A0
+const int flexPin1 = A1;
+const int flexPin2 = A2;
+const int flexPin3 = A3;
+const int flexPin4 = A4;
+
+//Variables:
+int value; //save analog value
+int value1;
+int value2;
+int value3;
+int value4;
+
+
+Servo myservo;
+Servo myservo1;
+Servo myservo2;
+Servo myservo3;
+Servo myservo4;
+
+int angle = 0;//chanegs angle value based on flex sensor reading
+int angle1 = 0;
+int angle2 = 0;
+int angle3 = 0;
+int angle4 = 0;
+
+
+
+
+void setup() {
+
+  Serial.begin(9600);
+  myservo.attach(9);
+  myservo1.attach(13);
+  myservo3.attach(10);
+  //myservo2.attach();
+  //myservo4.attach();
+
+
+}
+
+void loop() {
+
+  value = analogRead(flexPin);         //Read and save analog value from potentiometer
+  //Serial.println(value);               //Print value
+  angle = map(value, 145, 260, 180, 0);//Map value 0-1023 to 0-255 (PWM)
+  angle = constrain(angle, 0, 180);
+  Serial.print("finger1: ");
+  Serial.print(angle);
+  myservo.write(angle);
+  delay(40);
+  //flexpin1
+  value1 = analogRead(flexPin1);         //Read and save analog value from potentiometer
+  //Serial.println(value);               //Print value
+  angle1 = map(value1, 145, 275, 0, 180);//Map value 0-1023 to 0-255 (PWM)
+  angle1 = constrain(angle1, 0, 180);
+  Serial.print("\t finger1: ");
+  Serial.println(angle1);
+  myservo1.write(angle1);
+  delay(40);
+  /*//flexpin2
+    value2 = analogRead(flexPin2);         //Read and save analog value from potentiometer
+    //Serial.println(value);               //Print value
+    angle2 = map(value2, 718, 865, 0, 180);//Map value 0-1023 to 0-255 (PWM)
+    Serial.print("\t sensor2: ");
+    Serial.print(angle2);
+    myservo2.write(angle2);
+    delay(40);
+  */
+  //flexpin3
+  value3 = analogRead(flexPin3);         //Read and save analog value from potentiometer
+  //Serial.println(value);               //Print value
+  angle3 = map(value3, 145, 275, 0, 180);//Map value 0-1023 to 0-255 (PWM)
+  angle3 = constrain(angle3, 0, 180);
+  Serial.print("\t finger3: ");
+  Serial.println(angle3);
+  myservo3.write(angle3);
+  delay(40);
+}
+/*
+    //flexpin4
+    value4 = analogRead(flexPin4);         //Read and save analog value from potentiometer
+    //Serial.println(value);               //Print value
+    angle4 = map(value4, 715, 880, 0, 180);//Map value 0-1023 to 0-255 (PWM)
+    Serial.print("\t sensor4: ");
+    Serial.println(angle4);
+    myservo4.write(angle4);
+    delay(40);
+  }
+*/
